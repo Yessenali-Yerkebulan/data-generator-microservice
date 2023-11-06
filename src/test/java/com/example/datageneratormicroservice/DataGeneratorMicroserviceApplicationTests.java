ea7186005f4a46be2717dde6242cfc84cda86fec
yyessenali <yyessenali@greet-go.com>
2023-11-06 10:27:48 +0600

    initial commit

---
+++ src/test/java/com/example/datageneratormicroservice/DataGeneratorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datageneratormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
